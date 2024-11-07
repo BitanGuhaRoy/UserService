047eb539540bb9b3dda19c4d269e5f34feffdade
Bitan Guha Roy <bitanguharoy@gmail.com>
2024-11-07 23:50:04 +0530

    token getting generated from postman and made necessary changes in code

--- src/main/java/org/example/userservice/config/SecurityConfiguration.java
+++ src/main/java/org/example/userservice/config/SecurityConfiguration.java
@@ -5,7 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 @Configuration
-public class SecurityConfig {
+public class SecurityConfiguration {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
--- src/test/java/org/example/userservice/UserServiceApplicationTests.java
+++ src/test/java/org/example/userservice/UserServiceApplicationTests.java
@@ -1,13 +1,50 @@
 package org.example.userservice;
 
+import org.example.userservice.security.repository.ClientRepository;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.oauth2.core.AuthorizationGrantType;
+import org.springframework.security.oauth2.core.ClientAuthenticationMethod;
+import org.springframework.security.oauth2.core.oidc.OidcScopes;
+import org.springframework.security.oauth2.server.authorization.client.RegisteredClient;
+import org.springframework.security.oauth2.server.authorization.client.RegisteredClientRepository;
+import org.springframework.security.oauth2.server.authorization.settings.ClientSettings;
+import org.springframework.test.annotation.Commit;
+
+import java.util.UUID;
 
 @SpringBootTest
 class UserServiceApplicationTests {
 
+    @Autowired
+    private RegisteredClientRepository registeredClientRepository;
     @Test
     void contextLoads() {
     }
 
+    @Test
+    @Commit
+    void storageRegisterClientInDb()
+    {
+        RegisteredClient oidcClient = RegisteredClient.withId(UUID.randomUUID().toString())
+                .clientId("oidc-client")
+                .clientSecret("{noop}secret") // store encrypted "secret"
+                .clientAuthenticationMethod(ClientAuthenticationMethod.CLIENT_SECRET_BASIC)
+                .authorizationGrantType(AuthorizationGrantType.AUTHORIZATION_CODE)
+                .authorizationGrantType(AuthorizationGrantType.REFRESH_TOKEN)
+                .redirectUri("https://oauth.pstmn.io/v1/callback")
+                .postLogoutRedirectUri("https://oauth.pstmn.io/v1/callback")
+                .scope(OidcScopes.OPENID)
+                .scope(OidcScopes.PROFILE)
+                .scope("ADMIN")
+                .clientSettings(ClientSettings.builder().requireAuthorizationConsent(true).build())
+                .build();
+        registeredClientRepository.save(oidcClient);
+        // Postman
+        //Auth url : http://localhost:3030/oauth2/authorize
+        //Token url : http://localhost:3030/oauth2/token
+        //scope : ADMIN
+
+    }
 }
